2593bebabe9248797ffeac8549a95e8e14f9e032
Mireille_Koutchade <mireille.agbla@gmail.com>
2024-10-24 07:21:33 +0200

    commit

--- codeSVM/mes_chargements_modeles.R
+++
@@ -1,5 +0,0 @@
-# Ou charger le modèle à partir du fichier .rds
-
-svm_model_low <- readRDS("svm_model_low.rds")
-svm_model_mid <- readRDS("svm_model_mid.rds")
-svm_model_high <- readRDS("svm_model_high.rds")
